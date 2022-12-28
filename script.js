0fe987a0033cd1777f1cd87331b2a1083baa3d85
jefersonviana <jefersonv28@gmail.com>
2022-12-28 17:25:37 -0300

    Requisito 5 concluído

--- script.js
+++ script.js
@@ -1,20 +1,48 @@
 const main = document.querySelector('main');
+const color = document.getElementById('rgb-color');
+const container = document.createElement('section');
+main.appendChild(container);
+
+
+const drawCircle = () => {
+  const drawnCircle = document.querySelectorAll('.ball');
+  let sorteio = Math.floor(Math.random() * 6);
+  color.innerText = drawnCircle[sorteio].style.backgroundColor;
+};
+
 
 function randomColors() {
-  const string = '0123456789ABCDEF';
-  let createdColor = '#';
+  let generateColor = [];
   for (let index = 0; index < 6; index += 1) {
     const circle = document.createElement('div');
     circle.className = 'ball';
-    for (let index1 = 0; index1 < 6; index1 += 1) {
-      createdColor += string[Math.floor(Math.random() * 16)];
-      circle.style.backgroundColor = createdColor;
+    for (let index1 = 0; index1 < 3; index1 += 1) {
+      generateColor.push(Math.floor(Math.random() * 255));
     }
-    main.appendChild(circle);
-    createdColor = '#';
+    circle.style.backgroundColor = `rgb(${generateColor[0]} , ${generateColor[1]} , ${generateColor[2]})`;
+    generateColor = [];
+    container.appendChild(circle);
   }
+  drawCircle();
 };
 
 window.onload = () => {
   randomColors();
 };
+
+const paragraph = document.createElement('p');
+paragraph.id = 'answer';
+paragraph.innerText = 'Escolha uma cor';
+main.appendChild(paragraph);
+
+const validation = (event) => {
+  const alvo = event.target;
+  console.log(alvo.style.backgroundColor);
+  if (alvo.style.backgroundColor === color.innerText) {
+    paragraph.innerText = 'Acertou!';
+  } else {
+    paragraph.innerText = 'Errou! Tente novamente!';
+  }
+};
+
+container.addEventListener('click', validation)
