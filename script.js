b67a56bb7275b41b5a18270dace193a13ba42dbd
jefersonviana <jefersonv28@gmail.com>
2022-12-28 16:20:04 -0300

    Requisito 4 concluído

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
 const main = document.querySelector('main');
-for (let index = 0; index < 6; index += 1) {
-  const circle = document.createElement('div');
-  circle.className = 'ball';
-  main.appendChild(circle);
-}
+
+function randomColors() {
+  const string = '0123456789ABCDEF';
+  let createdColor = '#';
+  for (let index = 0; index < 6; index += 1) {
+    const circle = document.createElement('div');
+    circle.className = 'ball';
+    for (let index1 = 0; index1 < 6; index1 += 1) {
+      createdColor += string[Math.floor(Math.random() * 16)];
+      circle.style.backgroundColor = createdColor;
+    }
+    main.appendChild(circle);
+    createdColor = '#';
+  }
+};
+
+window.onload = () => {
+  randomColors();
+};
